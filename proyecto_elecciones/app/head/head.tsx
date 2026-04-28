9c5edc6ed5cc909d466e46f4899da54e69b62b88
Ariiiiii22 <321103097@pcpuma.acatlan.unam.mx>
2026-04-28 14:58:58 +0000

    cambios head

--- proyecto_elecciones/app/head/head.tsx
+++ proyecto_elecciones/app/head/head.tsx
@@ -1,5 +1,13 @@
+import styles from "./head/head.module.css"
+import Image from "next/image";
+
 export default function Head(){
     return(
-        <></>
+        <header className={styles.header}>
+      <div>
+        Esto es un header
+      </div>
+    </header>
     )
 }
+
--- proyecto_elecciones/app/page.tsx
+++ proyecto_elecciones/app/page.tsx
@@ -3,10 +3,9 @@ import Head from "./head/head.tsx";
 import styles from "./head/head.module.css"
 export default function Home() {
   return (
-    <header className={styles.header}>
-      <div>
-        Esto es un header
-      </div>
-    </header>
+    <>
+      <Head />
+    </>
   );
 }
+
