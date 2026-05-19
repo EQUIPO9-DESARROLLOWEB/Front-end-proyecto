7336832a139f4ddf4cb40127c972f82ec2c9d989
Luis Fernando Romero 10 <320584622@pcpuma.acatlan.unam.mx>
2026-05-19 14:16:26 +0000

    Corregir merge y actualizar Home

--- proyecto_elecciones/app/page.tsx
+++ proyecto_elecciones/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-import Resultados  from "./Resultados/page";
+import Resultados from "./Resultados/page";
+
 export default function Home() {
   return (
     <>
-      <Resultados />s
+      <Resultados />
     </>
   );
 }
